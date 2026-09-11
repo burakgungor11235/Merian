e7459ea02a17a33eceeab46494d8d44d1015680f
Burak Güngör <burak.gungor11235@gmail.com>
2026-09-11 15:59:07 +0300

    add basic lexing

---
+++ src/lexer.rs
@@ -0,0 +1,85 @@
+use logos::Logos;
+
+#[derive(Debug, PartialEq)]
+struct Heading<'a> {
+    parts: &'a str,
+    text: &'a str,
+}
+
+#[derive(Logos, Debug, PartialEq)]
+enum Token<'a> {
+    #[token("\n\n")]
+    ParagraphBreak,
+
+    #[token("\n")]
+    Newline,
+
+    #[regex(r"#[0-9]+(?:\.[0-9]+)*")]
+    HeadingMarker(&'a str),
+
+    #[token("**")]
+    Bold,
+    #[token("__")]
+    Italic,
+    #[token("==")]
+    Underline,
+    #[token("~~")]
+    Striketrhu,
+
+    #[regex(r"[a-zA-Z]+")]
+    Text(&'a str),
+}
+
+#[cfg(test)]
+mod tests {
+
+    macro_rules! wrap {
+        ($t:expr) => {
+            Some(Ok($t))
+        };
+    }
+
+    use crate::lexer::Token::*;
+
+    use super::*;
+    #[test]
+    fn simple() {
+        let mut l = Token::lexer("simple\n\ntext");
+        assert_eq!(l.next(), wrap!(Token::Text("simple")));
+        assert_eq!(l.next(), wrap!(Token::ParagraphBreak));
+        assert_eq!(l.next(), wrap!(Token::Text("text")));
+    }
+
+    #[test]
+    fn heading() {
+        let mut lexer = Token::lexer(
+            "#1\n\
+     #1.1\n\
+     #1.1.1\n\
+        ",
+        );
+
+        assert_eq!(lexer.next(), Some(Ok(Token::HeadingMarker("#1"))));
+        assert_eq!(lexer.next(), Some(Ok(Token::Newline)));
+        assert_eq!(lexer.next(), Some(Ok(Token::HeadingMarker("#1.1"))));
+        assert_eq!(lexer.next(), Some(Ok(Token::Newline)));
+        assert_eq!(lexer.next(), Some(Ok(Token::HeadingMarker("#1.1.1"))));
+        assert_eq!(lexer.next(), Some(Ok(Token::Newline)));
+    }
+    #[test]
+    fn bold_italic() {
+        let mut lexer = Token::lexer("**start**__end__~~lol~~==important==");
+        assert_eq!(lexer.next(), wrap!(Bold));
+        assert_eq!(lexer.next(), wrap!(Text("start")));
+        assert_eq!(lexer.next(), wrap!(Bold));
+        assert_eq!(lexer.next(), wrap!(Italic));
+        assert_eq!(lexer.next(), wrap!(Text("end")));
+        assert_eq!(lexer.next(), wrap!(Italic));
+        assert_eq!(lexer.next(), wrap!(Striketrhu));
+        assert_eq!(lexer.next(), wrap!(Text("lol")));
+        assert_eq!(lexer.next(), wrap!(Striketrhu));
+        assert_eq!(lexer.next(), wrap!(Underline));
+        assert_eq!(lexer.next(), wrap!(Text("important")));
+        assert_eq!(lexer.next(), wrap!(Underline));
+    }
+}
