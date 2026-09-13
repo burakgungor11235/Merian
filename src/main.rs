b9c485f78f9f7fd47520e34273cb8e0fffdb1744
Burak Güngör <burak.gungor11235@gmail.com>
2026-09-13 17:20:17 +0300

    upgrades ppl upgrades

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,7 @@ use std::fs;
 use std::io::{self, Write};
 use std::path::PathBuf;
 
-use crate::lexer::Token;
-use crate::{assembler::Assembler, parser::parse};
 use clap::Parser;
-use logos::Logos;
 
 mod assembler;
 mod ast;
@@ -21,61 +18,82 @@ struct Cli {
     #[arg(short = 'o', long = "output", value_name = "FILE")]
     output: Option<PathBuf>,
 
-    /// Dump the Abstract Syntax Tree (AST) for debugging purposes
+    /// Dump the AST
     #[arg(long = "dump-ast")]
     dump_ast: bool,
 
     /// Dump tokens
     #[arg(long = "dump-tokens")]
     dump_tokens: bool,
+
+    /// Cat
+    #[arg(long = "cat")]
+    cat: bool,
 }
 
 fn main() {
     let cli = Cli::parse();
+    run(&cli);
+}
 
+fn handle_cat() {
+    #[cfg(target_os = "windows")]
+    let _ = std::process::Command::new("cmd")
+        .args(["/C", "start", "https://cataas.com/cat/gif"])
+        .spawn();
+
+    #[cfg(target_os = "macos")]
+    let _ = std::process::Command::new("open")
+        .arg("https://cataas.com/cat/gif")
+        .spawn();
+
+    #[cfg(target_os = "linux")]
+    let _ = std::process::Command::new("xdg-open")
+        .arg("https://cataas.com/cat/gif")
+        .spawn();
+}
+
+fn run(cli: &Cli) {
     let source = fs::read_to_string(&cli.input).unwrap_or_else(|err| {
-        eprintln!(
-            "markupc: fatal error: cannot read input file '{}': {}",
-            cli.input.display(),
-            err
-        );
+        eprintln!(" cannot read input file '{}': {err}", cli.input.display(),);
         std::process::exit(1);
     });
 
-    let doc = parse(&source);
-    if cli.dump_tokens {
-        println!(
-            "{:#?}",
-            Token::lexer(&source)
-                .map(|f| format!("{:?}", (f)))
-                .collect::<Vec<String>>()
-        );
+    if cli.cat {
+        handle_cat();
     }
-    if cli.dump_ast {
-        println!("{:#?}", doc);
+    let doc = parser::parse(&source);
+
+    if cli.dump_tokens {
+        use lexer::Token;
+        use logos::Logos;
+
+        let tokens: Vec<_> = Token::lexer(&source).map(|t| format!("{t:?}")).collect();
+        println!("{tokens:#?}");
         return;
     }
 
-    let asm = Assembler::default();
-    let html_output = asm.assemble(&doc);
+    if cli.dump_ast {
+        println!("{doc:#?}");
+        return;
+    }
 
-    match cli.output {
+    let html_output = assembler::Assembler::default().assemble(&doc);
+
+    match &cli.output {
         Some(out_path) => {
-            // User provided an `-o` flag, write to file
-            fs::write(&out_path, html_output).unwrap_or_else(|err| {
+            fs::write(out_path, &html_output).unwrap_or_else(|err| {
                 eprintln!(
-                    "markupc: fatal error: cannot write to output file '{}': {}",
+                    "error: cannot write to output file '{}': {err}",
                     out_path.display(),
-                    err
                 );
                 std::process::exit(1);
             });
         }
         None => {
-            // shit the contents to stdout
-            let mut stdout = io::stdout();
-            if let Err(err) = stdout.write_all(html_output.as_bytes()) {
-                eprintln!("markupc: fatal error: failed to write to stdout: {}", err);
+            // throw the contents to stdout
+            if let Err(err) = io::stdout().write_all(html_output.as_bytes()) {
+                eprintln!("error: failed to write to stdout (how tf?): {err}");
                 std::process::exit(1);
             }
         }
